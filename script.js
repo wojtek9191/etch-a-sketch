a0ff841953c7b60b96926654461dc0abcb0ef198
Wojciech Miczolek <wojciechm2291@gmail.com>
2023-05-14 18:29:28 +0100

    Add query selector on the select button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('hi');
-
 
 function createDivs(size) {
 
@@ -12,20 +10,42 @@ let divsNum = size * size;
 for (let i = 0; i < divsNum; i++) {
     let div = document.createElement("div");
     div.onmouseover = function() {
-        div.style.backgroundColor = "red"
+    div.style.backgroundColor = "red";
     }
     container.appendChild(div);
 }
 
-
-
 }
 
-createDivs(100);
+//createDivs(16);
 
 
+function selectSize() {
+    let userInput = prompt("Please provide a number between 1 and 100 to select the size of the board", 16);
+    if(userInput < 1) {
+    alert("Oops! The number must be greater 0.");
+}
+    else if(userInput > 100) {
+    alert("Oops! The number cannot be greather than 100.");
+}
+    else if(userInput == !isNaN) {
+    alert("Oops! Please provide a valid number.");
+}
+    
+    return userInput;
+}
 
 
+const selectButton = document.querySelector("#size");
+selectButton.addEventListener("click", function() {
+
+let boardSize = selectSize();
+createDivs(boardSize);
+
+})
+
+
+//selectSize();
 
 
 
